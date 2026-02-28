17bb1a359fecf6d6e9d919c34ad8aad05ad0cb41
Captain1306 <yaariyaar06@gmail.com>
2026-02-27 16:46:57 -0800

    Add list practice file

---
+++ list_test.py
@@ -0,0 +1,18 @@
+info = ["Sheri" , 20 , "Failed"]
+print(info)
+info[2] = "Pass"
+print(info)
+marks = [60,70,90,100]
+print(marks)
+marks.append(99)
+print(marks)
+marks.insert(1,19)
+print(marks)
+marks.sort()
+print(marks)
+marks.sort(reverse=True)
+print(marks)
+marks.pop(2)
+print(marks)
+marks.remove(99)
+print(marks)
