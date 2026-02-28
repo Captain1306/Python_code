b4bafffe6b4c3923ba0cf6b40aa9a7e5713a543e
Captain1306 <yaariyaar06@gmail.com>
2026-02-28 16:31:50 +0500

    takken and print list from user and check if list is palindrom

--- list_test.py
+++ list_test.py
@@ -16,3 +16,18 @@ marks.pop(2)
 print(marks)
 marks.remove(99)
 print(marks)
+movies = []
+m1 = input("ENter movie 1")
+movies.append(m1)
+m2 = input("ENter movie 2")
+movies.append(m2)
+m3 = input("Enter movie 3")
+movies.append(m3)
+print(movies)
+pali = [1,2,2,1]
+pali1=pali.copy
+pali1.reverse()
+if(pali==pali1):
+    print("yes its palindrome")
+else:
+    print("No its not palindrome")
