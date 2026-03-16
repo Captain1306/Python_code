0c9e56825da2914c8bd14cf61d9f1b5bc06ab02d
Captain1306 <yaariyaar06@gmail.com>
2026-03-17 00:50:24 +0500

    arithmetic operations on array

---
+++ arith_arr.py
@@ -0,0 +1,14 @@
+import numpy as np
+var = np.array([1,2,3,4])
+sum_arr=np.add(var,3)
+print(sum_arr)
+
+var2 = np.array([5,6,7,8])
+sum_2var=np.add(var,var2)
+print(sum_2var)
+
+sub_2var=np.subtract(var,var2)
+print(sub_2var)
+
+div_2var=np.divide(var,var2)
+print(div_2var)
