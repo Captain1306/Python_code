9d3739d21e907ed8d1170e10ccf4be222d866c7a
Captain1306 <yaariyaar06@gmail.com>
2026-03-30 20:06:48 +0500

    functions on array

---
+++ func_arr.py
@@ -0,0 +1,2 @@
+import numpy as np
+x=np.array([10,40,80,90,30,5])
--- split_arr.py
+++ split_arr.py
@@ -1,7 +1,7 @@
 import numpy as np
-# ar=np.array([1,2,3,4,5,6])
-# sp=np.array_split(ar,3)
-# print(sp)
+ar=np.array([1,2,3,4,5,6])
+sp=np.array_split(ar,3)
+print(sp)
 ar2=np.array([[1,2],[3,4],[5,6]])
 sp2=np.array_split(ar2,3)
 print(sp2)
