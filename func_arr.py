774d54f84b27452d0608905714c929ed55468110
Captain1306 <yaariyaar06@gmail.com>
2026-04-02 10:55:35 -0700

    insertion and deletion program

--- func_arr.py
+++ func_arr.py
@@ -1,15 +1,15 @@
 import numpy as np
-# var=np.array([11,41,80,91,30,5])
-# #searching in array
-# x=np.where(var==80)
-# print(x)
-# #finding even numbers in array
-# y=np.where(var%2==0)
-# print(y)
-# var2=np.array([1,2,3,4,5])
-# #searching using binary
-# z=np.searchsorted(var2,4)
-# print(z)
+var=np.array([11,41,80,91,30,5])
+#searching in array
+x=np.where(var==80)
+print(x)
+#finding even numbers in array
+y=np.where(var%2==0)
+print(y)
+var2=np.array([1,2,3,4,5])
+#searching using binary
+z=np.searchsorted(var2,4)
+print(z)
 arr=np.array([3,20,10,5,8,1])
 arr=np.sort(arr)
 print(arr)
