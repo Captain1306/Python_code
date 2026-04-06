29dfce8d4cf12a9e524e539ec5ae4870688081bb
Captain1306 <yaariyaar06@gmail.com>
2026-04-06 09:30:44 -0700

    new changes

--- broadcasting.py
+++ broadcasting.py
@@ -1,9 +1,10 @@
 import numpy as np
-
 var1=np.array([1,2,3])
+print(var1)
 print(var1.shape)
 
 var2=np.array([[1],[2],[3]])
+print(var2)
 print(var2.shape)
 
 print(var1+var2)
--- ins_del.py
+++ ins_del.py
@@ -1,4 +1,12 @@
 import numpy as np
 y=np.array([1,2,3,4,5])
-y=np.append(10,2)
-print(y)
+y2=np.array([[10,20],[30,40],[50,60]])
+x2=np.insert(y2,1,1000,axis=1)
+print(x2)
+x=np.insert(y,4,100)
+#delete program
+print(x)
+z=np.delete(y,1)
+print(z)
+z1=np.delete(y2,1)
+print(z1)
