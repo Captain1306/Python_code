2180761eb64677d088065f86645fcff1b3450766
Captain1306 <yaariyaar06@gmail.com>
2026-04-07 09:49:10 -0700

    Introduction to pandas

---
+++ pandas/series.py
@@ -0,0 +1,7 @@
+import pandas as pd
+# x=[1,2,3,4,5]
+# a=pd.Series(x)
+# print(a)
+dict={"Name":['python','java','c++'],"ranking":['1','2','3'],"user friendly":[True,False,False]}
+b=pd.Series(dict)
+print(b)
