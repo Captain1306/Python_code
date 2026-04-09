602c9f00f8c050fca0673d7cf2c0bc846791e19b
Captain1306 <yaariyaar06@gmail.com>
2026-04-09 09:36:38 -0700

    new changes

--- pandas/series.py
+++ pandas/series.py
@@ -1,7 +1,8 @@
 import pandas as pd
-# x=[1,2,3,4,5]
-# a=pd.Series(x)
-# print(a)
+x=[1,2,3,4,5]
+a=pd.Series(x)
+print(a)
 dict={"Name":['python','java','c++'],"ranking":['1','2','3'],"user friendly":[True,False,False]}
 b=pd.Series(dict)
 print(b)
+print("hello")
