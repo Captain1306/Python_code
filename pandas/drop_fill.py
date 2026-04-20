9c2ae9336b174852e0555b70adebbcf1a8bc823f
Captain1306 <yaariyaar06@gmail.com>
2026-04-20 03:02:09 -0700

    new changes

--- pandas/drop_fill.py
+++ pandas/drop_fill.py
@@ -1,7 +1,7 @@
 import pandas as pd
 var1=pd.read_csv("D:\\Programming\\test_sub.csv")
-# print(var1)
-# print(var1.dropna())
-# print(var1.dropna(inplace=True))
-# print(var1.dropna(thresh=1))
+print(var1)
+print(var1.dropna())
+print(var1.dropna(inplace=True))
+print(var1.dropna(thresh=1))
 print(var1.fillna("python"))
