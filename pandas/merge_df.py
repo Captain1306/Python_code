82dd494dd01d8be0e86a2fb2f44e6256f2406c13
Captain1306 <yaariyaar06@gmail.com>
2026-04-23 20:16:05 -0700

    merging dataframes

--- pandas/merge_df.py
+++ pandas/merge_df.py
@@ -1,9 +1,18 @@
 import pandas as pd
-l1={"A":[1,2,3,4,5],
-    "B":[6,7,8,9,10]}
-l2={"A":[1,2,3,4,5],
-    "C":[11,12,13,14,15]}
-var1=pd.DataFrame(l1)
-var2=pd.DataFrame(l2)
-var3=pd.merge(var1,var2,on="A")
-print(var3)
+# l1={"A":[1,2,3,4,5],
+#     "B":[6,7,8,9,10]}
+# l2={"A":[1,2,3,4,5],
+#     "C":[11,12,13,14,15]}
+# var1=pd.DataFrame(l1)
+# var2=pd.DataFrame(l2)
+# var3=pd.merge(var1,var2,on="A")
+# print(var3)
+R1={"X":[1,2,3,4,5],
+    "Y":[6,7,8,9,10]}
+R2={"X":[1,2,3,4,5],
+    "Y":[11,12,13,14,15]}
+da=pd.DataFrame(R1)
+db=pd.DataFrame(R2)
+dc=pd.merge(da,db,left_index=True,right_index=True)
+print(dc)
+
